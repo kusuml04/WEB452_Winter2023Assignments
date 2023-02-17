6ce0239c50f8008cca109b9b93106e1eb0402f90
kusuml04 <KK670@myscc.ca>
2023-02-17 01:38:52 -0500

    Create SeedData.cs Update SeedData.cs by adding Lamps properties. and Update the Program.cs file .

---
+++ Lamps/Models/SeedData.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Lamps.Data;
+using System;
+using System.Linq;
+
+namespace Lamps.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new LampsContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<LampsContext>>()))
+            {
+                // Look for any Lamps.
+                if (context.Lamp.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Lamp.AddRange(
+                    new Lamp
+                    {
+                        Name = "Incandescent Lamps",
+                        Brightness = "Luminous Flux",
+                        Efficiency = "Low Efficiency",
+                        Shapes = "Bulb Shape",
+                        ColorTemperature = "Bulb Shape",
+                        Rating = 1
+                    },
+
+                    new Lamp
+                    {
+                        Name = "Fluorescent Lamps",
+                        Brightness = "Bright Light",
+                        Efficiency = "High Efficiency",
+                        Shapes = "Tubular",
+                        ColorTemperature = "Kelvin Scale",
+                        Rating = 4
+                    },
+
+                    new Lamp
+                    {
+                        Name = "LED Lamps",
+                        Brightness = "High",
+                        Efficiency = "Energy Efficient",
+                        Shapes = "Bulb, Tube etc",
+                        ColorTemperature = "Variable",
+                        Rating = 1
+                    },
+
+                    new Lamp
+                    {
+                        Name = "Oil Lamps",
+                        Brightness = "Warm Glow",
+                        Efficiency = "Low Efficiency",
+                        Shapes = "Cylindrical or Conical",
+                        ColorTemperature = "Warm Yellow",
+                        Rating = 1
+                    },
+                    new Lamp
+                    {
+                        Name = "Salt Lamps",
+                        Brightness = "Soft Light",
+                        Efficiency = "Low Efficiency",
+                        Shapes = "Spherical",
+                        ColorTemperature = "Warm",
+                        Rating = 1
+                    },
+                    new Lamp
+                    {
+                        Name = "Neon Lamps",
+                        Brightness = "Bright Glow",
+                        Efficiency = "Low Efficiency",
+                        Shapes = "Tubular",
+                        ColorTemperature = "No specific Temperature",
+                        Rating = 1
+                    },
+                    new Lamp
+                    {
+                        Name = "Halogen Lamps",
+                        Brightness = "High",
+                        Efficiency = "Low Efficiency",
+                        Shapes = "Bulb, Tube etc",
+                        ColorTemperature = "warme",
+                        Rating = 1
+                    },
+                    new Lamp
+                    {
+                        Name = "Floor Lamps",
+                        Brightness = "High",
+                        Efficiency = "Energy Efficient",
+                        Shapes = "Tall",
+                        ColorTemperature = "Neutral",
+                        Rating = 5
+                    },
+                    new Lamp
+                    {
+                        Name = "Buffet Lamps",
+                        Brightness = "Dim",
+                        Efficiency = "Energy Efficient",
+                        Shapes = "Bulb, Tube etc",
+                        ColorTemperature = "Warm/Dim",
+                        Rating = 4
+                    },
+                    new Lamp
+                    {
+                        Name = "Desk Lamps",
+                        Brightness = "High",
+                        Efficiency = "Energy Efficient",
+                        Shapes = "Adjustable",
+                        ColorTemperature = "Cool",
+                        Rating = 5
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Lamps/Readme.txt
+++ Lamps/Readme.txt
@@ -1,7 +1,7 @@
 ﻿Product was assigned for Assignment on 2 Feb, 2023.
 Then i took some days to think about  my product 
 
-Start Project on 2023-02-16    0230
+Start Project on 2023-02-16    1430
 At first I create a project named with my assigned product that is Lamps by
 selecting Individual Accounts.
 
@@ -11,7 +11,7 @@ After creating project I test my project . It was perfect till this time
 
 In the next step I add HelloWorldController.cs  inside  Controllers/HelloWorldController.cs.
 The do coding in HelloWorldController.cs.
-After that I test by code by IIS Express . It is running till this time      0330
+After that I test by code by IIS Express . It is running till this time      1530
 After that test again on localhost by /HelloWorld/Welcome .   
 
 
@@ -31,10 +31,10 @@ using Microsoft.EntityFrameworkCore;
 
 Then I reached on Scaffold Page 
 oooooohh shitttt got an error  
-something like download microsoft.visualstudio.web.codegenerator      0916
+something like download microsoft.visualstudio.web.codegenerator      2116
 
 ****To solve this error i modify something in my Visual Studio Installer 
-Then try to scaffold again,   huuuuuu luckily successful this time .      10.00 ****
+Then try to scaffold again,   huuuuuu luckily successful this time .      2200 ****
 
 Use the EF Core Migrations feature to create the database
 
@@ -42,3 +42,15 @@ In the PMC, enter the following commands:
 
 Add-Migration InitialCreate             1030
 Update-Database
+
+***Took Break for some time ****
+
+then create as class named as SeedData.cs in the Models folder .
+Update the code of SeedData.cs file with adding Lamps information .
+
+
+After that Update my Program.cs file .
+
+Yeaahhh! Finally Test my data .   Hurraaah! successfuly got my Lamps properties.          0133
+
+thats it for today . Rest part will do next morning.    Pause Project  0135
